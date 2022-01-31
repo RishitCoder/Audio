d33765327239d594d3e2914cb29510cc2ea00cd4
Rishit Sharma <78579590+RishitCoder@users.noreply.github.com>
2022-01-31 16:11:38 +0530

    Add files via upload

---
+++ s.js
@@ -0,0 +1,36 @@
+$("#yo").on("click" , function(){
+    $("#this").text("Hello World")
+    
+})
+
+$("#y").on("click" , function(){
+    $("#this").fadeOut()
+})
+
+$("#ye").on("click" , function(){
+    $("#this").fadeIn()
+})
+
+$("#ys").on("click" , function(){
+    $("#this").text("Hello")
+    
+})
+
+$("#button").on("click" , function(){
+    $("#n").attr("src" , "https://media2.giphy.com/media/MugllcR6Gq8Y8/200.gif")
+})
+
+$("#buttonyo").on("click" , function(){
+    $("#n").attr("src" , "https://qph.fs.quoracdn.net/main-qimg-2a4e9ac850c476e1300d7511d6984bb3")
+})
+
+$("#yes").on("click" , function(){
+    $("#n").fadeOut()
+})
+
+$("#yde").on("click" , function(){
+    $("#n").fadeIn()
+})
+// Made by Rishit Sharma
+// It was good
+
